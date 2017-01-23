c8a8206a4087bb184855982030e7f96f0e8be464
e-inquirer <sorour.a@gmail.com>
2017-01-23 15:12:15 -0500

    LoginHandler class test stub.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import webapp2
+import random
 
 class MainHandler(webapp2.RequestHandler):
-        def get(self):
-            self.response.write('Hello world!')
+    def get(self):
+        header = "<h1>Fortune Cookie</h1>"
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        lucky_number = random.randint(1, 100)
+        number_sentence = 'Your lucky number: ' + str(lucky_number) 
+        number_paragraph = "<p>" + number_sentence + "</p>"
+        self.response.write(header + number_paragraph)
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Thanks for trying to log in!")
+
+routes = [
+    ('/', MainHandler),
+    ('/login', LoginHandler)
+]
+
+app = webapp2.WSGIApplication(routes, debug=True)
