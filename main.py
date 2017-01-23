cb3bce872f8365a8399096154d6f6a4dc79642dc
e-inquirer <sorour.a@gmail.com>
2017-01-23 16:09:26 -0500

    implemented lucky number and random fortune and refresh button

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import webapp2
 import random
 
+def getRandomFortune():
+    # list of possible fortunes
+    fortunes = [
+            "I see much code in your future",
+            "Consider eating more fortune cookies",
+            "You have tamed the mighty Python, now you must free it onto the Great Spider's Web!"]
+    
+    #randomly select one of the fortunes 
+    index = random.randint(0, 2)
+    return fortunes[index]
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = "<h1>Fortune Cookie</h1>"
 
-        lucky_number = random.randint(1, 100)
-        number_sentence = 'Your lucky number: ' + str(lucky_number) 
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = "<strong>" + str(random.randint(1, 100)) + "</strong>"
+        number_sentence = 'Your lucky number: ' + lucky_number 
         number_paragraph = "<p>" + number_sentence + "</p>"
-        self.response.write(header + number_paragraph)
 
-class LoginHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write("Thanks for trying to log in!")
+        cookie_again_button = "<a href='.'><button>Another cookie please!</button></a>"
 
-routes = [
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+
+        self.response.write(content)
+
+
+app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/login', LoginHandler)
-]
-
-app = webapp2.WSGIApplication(routes, debug=True)
+], debug=True)
